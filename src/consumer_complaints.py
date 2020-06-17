49d0404c1b106156f333cd79158042b7bf1c5bdc
raveenapooja <66836884+raveenapooja@users.noreply.github.com>
2020-06-17 17:06:12 -0400

    Update consumer_complaints.py

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -31,37 +31,41 @@ def filter_year_from_date(v):
     return(v)
 
 if __name__ == '__main__':
+    #main function with the function calls and operations with input and output files
     input_file = sys.argv[1]
     output_file = sys.argv[2]
+    #using the csv attributes for reading from and writing into files
     with open(input_file,"r",encoding='UTF-8') as input:
         rdr= csv.reader( input )
         with open(output_file,"w", newline='') as output:
             wtr= csv.writer( output )
+            #filtering out only the Product, Date and Company fields
             for r in rdr:
                 wtr.writerow((r[0],r[1],r[7]))
-            
-    f = open(output_file,"r")
-    next(f)
-    csv_f = csv.reader(f)
-    v=list(csv_f)
     
-    M = list(zip(*(filter_year_from_date(v))))
-    M[0], M[1] = M[1], M[0]
-    filtered_fields=[list(t) for t in zip(*M)]
+    #All operations performed from the output file contents
+    f = open(output_file,"r")
+    next(f)                     #skipping the name row
+    csv_f = csv.reader(f)
+    v=list(csv_f)               #convering the contents of the file into a list
+    
+    M = list(zip(*(filter_year_from_date(v))))              #fitering out the year from the date field in the entire input
+    M[0], M[1] = M[1], M[0]                                 #interchanging the first and second column to be in the format (Product,Year)
+    filtered_fields=[list(t) for t in zip(*M)]              #Adding the (Product,Year) fields to the Company field
 
-    for r in interchange_columns_count(filtered_fields):
-        r[0]=list(r[0])
+    for r in interchange_columns_count(filtered_fields):        #iterating through the list with the number of complaints recieved in a year
+        r[0]=list(r[0])                                           
         r[0].append(r[1])
-        ordered.append(r[0])
+        ordered.append(r[0])                                    #Adding the count as another column in the main list
 
-    for r in interchange_columns_count(set((map(tuple,filtered_fields)))):
-        third_column.append(r[1])
+    for r in interchange_columns_count(set((map(tuple,filtered_fields)))):    #counting the number of companies with atleast one complaint per product
+        third_column.append(r[1])                                             #adding the count to another list
     
     for r in ordered:
-        r.append(third_column[i])
+        r.append(third_column[i])                                           #appending the count to the last column of the main list
         i+=1
             
     with open(output_file,"w", newline='') as output:
             wtr= csv.writer( output )
-            for r in percentage_double_quotes(ordered):
-                wtr.writerow(r)
+            for r in percentage_double_quotes(ordered):                        #converting everything to lowercase and adding percentage and adding quotes
+                wtr.writerow(r)                                                #writing the final list to the output file
